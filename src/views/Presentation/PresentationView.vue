268ca854cad61c53b7411dedab00f3eb7df7846b
Able <134662899+Ablebangbang@users.noreply.github.com>
2023-08-01 14:04:26 +0800

    123

--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -2,8 +2,7 @@
 import { onMounted, onUnmounted } from "vue";
 
 //example components
-import NavbarDefault from "../..//examples/navbars/NavbarDefault.vue";
-import DefaultFooter from "../../examples/footers/FooterDefault.vue";
+
 import Header from "../../examples/Header.vue";
 // import FilledInfoCard from "../../examples/cards/infoCards/FilledInfoCard.vue";
 
@@ -42,13 +41,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
-        <NavbarDefault :sticky="true" />
-      </div>
-    </div>
-  </div>
+  
   <Header>
     <div
       class="page-header min-vh-75"
@@ -313,5 +306,5 @@ onUnmounted(() => {
       </div>
     </div>
   </div>
-  <DefaultFooter />
+  
 </template>
